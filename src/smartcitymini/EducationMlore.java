ca43cb299d69deba6c3423ff74c3aee77192c90a
Harshita Vidapanakal <65297242+hvgh88@users.noreply.github.com>
2022-05-01 22:56:35 +0530

    Update EducationMlore.java

--- src/smartcitymini/EducationMlore.java
+++ src/smartcitymini/EducationMlore.java
@@ -1,5 +1,38 @@
 package smartcitymini;
 
+
 public class EducationMlore {
 
+	public EducationMlore() {
+		
+		String s1 = "PRESTIGE International School";
+		String s2 = "Lourdes Central School";
+		String s3 = "SHARADA VIDYALAYA Kodialbail";
+		String s4 = "Mount Carmel Central School";
+
+		String simg1 = ".//Mangalore//PRESTIGESchool.jpg";
+		String simg2 = ".//Mangalore//lourdescentralschool.jpg";
+		String simg3 = ".//Mangalore//SHARADAVIDYALAYAKodialbail.jpg";
+		String simg4 = ".//Mangalore//MountCarmelCentralSchool.jpg";
+
+		String c1 = "Karnataka College";
+		String c2 = "St Joseph Engineering College";
+
+		String u1 = "Yenepoya University";
+		String u2 = "NITK";
+		String u3 = "Mangalore University";
+
+		String cimg1 = ".//Mangalore//karnataka college.pngS";
+		String cimg2 = ".//Mangalore//stjoseph.png";
+
+		String uimg1 = ".//Mangalore//yenepoya-university.jpg";
+		String uimg2 = ".//Mangalore//nitk.jpg";
+		String uimg3 = ".//Mangalore//mlore-university.jpg";
+		
+		String cityname = "Mangalore";
+		
+		
+		Education e = new Education(s1,s2,s3,s4,c1,c2,u1,u2,u3,simg1,simg2,simg3,simg4,cimg1,cimg2,uimg1, uimg2, uimg3, cityname);
+
+	}
 }
