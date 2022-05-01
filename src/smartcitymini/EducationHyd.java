8367f872e05a19e4c749617d17b54717c7300811
Harshita Vidapanakal <65297242+hvgh88@users.noreply.github.com>
2022-05-01 22:56:12 +0530

    Update EducationHyd.java

--- src/smartcitymini/EducationHyd.java
+++ src/smartcitymini/EducationHyd.java
@@ -1,5 +1,38 @@
 package smartcitymini;
 
+
 public class EducationHyd {
 
+	public EducationHyd() {
+		
+		String s1 = "Chirec International School";
+		String s2 = "International School of Hyderabad";
+		String s3 = "Glendale Academy";
+		String s4 = "Sreenidhi International School";
+
+		String simg1 = ".//img//Hyderabad//education//ChirecInternationalSchool.jpg";
+		String simg2 = ".//img//Hyderabad//education//ish.jpg";
+		String simg3 = ".//img//Hyderabad//education//glendaleacademy.png";
+		String simg4 = ".//img//Hyderabad//education//sreenidhi.jpg";
+
+		String c1 = "Osmania University";
+		String c2 = "BITS Pilani Hyderabad ";
+
+		String u1 = "IIT Hyderabad";
+		String u2 = "IIIT Hyderabad";
+		String u3 = "Hyderabad Central UNIVERSITY";
+
+		String cimg1 = ".//img//Hyderabad//education//osmaniauniversity.png";
+		String cimg2 = ".//img//Hyderabad//education//bitspilanihyderabad.png";
+		
+		String uimg1 = ".//img//Hyderabad//education//iithyderabad.jpg";
+		String uimg2 = ".//img//Hyderabad//education//iiithyderabad.jpg";
+		String uimg3 = ".//img//Hyderabad//education//hyderabadcentraluniversity.jpg";
+		
+		String cityname = "Hyderabad";
+		
+		
+		Education e = new Education(s1,s2,s3,s4,c1,c2,u1,u2,u3,simg1,simg2,simg3,simg4,cimg1,cimg2,uimg1, uimg2, uimg3, cityname);
+
+	}
 }
